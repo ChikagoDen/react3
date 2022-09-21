92a3349182307343b84b27fbf79794d3644c4a46
Highload <perepelkin-da@yandex.ru>
2022-09-21 22:08:37 +0600

    dz3-3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 import {useState} from "react";
-import {Box, Button, Fab, FormControl, Input, List, ListItem, ListItemText} from "@mui/material";
+import {Box, Button, Input, List, ListItem, ListItemText, FormGroup, Grid} from "@mui/material";
 import  Message from "./Message";
 
 export function App() {
+    const chatList =[
+        { id:'1',chatName:"Чат1"},
+        { id:'2',chatName:"Чат2"},
+        { id:'3',chatName:"Чат3"},
+    ]
     const [messagesList, setMessagesList] = useState([]);
+
     let [form, setForm] = useState({
         text: '',
         author: '',
@@ -29,7 +34,7 @@ export function App() {
 
     return(
         <Box component="div" sx={{ border: '1px dashed grey' }}>
-            <FormControl>
+            <FormGroup>
                 <Box component="label" sx={{ padding: '15px' }}>
                     Текст:
                     <Input sx={{ border: '1px solid green', margin: '15px'}} placeholder="Введите текст"  autoFocus={true}  value={form.text} name="text" onChange={handleUpdateInput}></Input>
@@ -39,15 +44,29 @@ export function App() {
                     <Input sx={{ border: '1px solid green', margin: '15px'}} placeholder="Введите свое имя" value={form.author} name="author" type="text" onChange={handleUpdateInput}></Input>
                 </Box>
                 <Button variant="contained" color="success" onClick={handleSubmit}>Отправить</Button>
-        </FormControl>
-            <List>
-                {messagesList.map(message =>{
-                    return(
-                        <ListItem key={message.id}>
-                            <ListItemText primary={"Сообщение:"+message.text} secondary={"Автор:"+message.author}></ListItemText>
-                        </ListItem>)
-                })}
-            </List>
+        </FormGroup>
+            <Grid  container spacing={2}>
+                <Grid  item xs={4}>
+                    <List>
+                        {chatList.map(chat =>{
+                            return(
+                                <ListItem key={chat.id}>
+                                    {chat.chatName}
+                                </ListItem>)
+                        })}
+                    </List>
+                </Grid>
+                <Grid item xs={8}>
+                    <List>
+                        {messagesList.map(message =>{
+                            return(
+                                <ListItem key={message.id}>
+                                    <ListItemText primary={"Сообщение:"+message.text} secondary={"Автор:"+message.author}></ListItemText>
+                                </ListItem>)
+                        })}
+                    </List>
+                </Grid>
+            </Grid>
         </Box>
     )
 }
